eaffa9a88156d337e4520a69579d7645b4aadfba
fatimarafiqui <fatima.rafiqui@gmail.com>
2017-05-29 01:05:18 +0530

    added environment variables

--- includes/settings.php
+++ includes/settings.php
@@ -6,23 +6,23 @@
  * Note       : Please do not commit your credentials
  */
 $_settings['reCaptcha'] = array();
-$_settings['reCaptcha']['client_key'] = '6LeAOhUUAAAAAKxj0kEhUUdxTGw4SQI-7LbTNHso';
-$_settings['reCaptcha']['server_key'] = '6LeAOhUUAAAAAO4R5aYPuOXlmqlIILm9FWjHtjTK';
+$_settings['reCaptcha']['client_key'] = getenv('client_key') ?? '';
+$_settings['reCaptcha']['server_key'] = getenv('server_key') ?? '';
 
 $_settings['twilio'] = array();
-$_settings['twilio']['account_sid'] = 'AC02b5e7eff09d776e4fa15963cbdecf8e';
-$_settings['twilio']['auth_token'] = '6d8d7004108d2ff1fcb4dccbb357edcb';
-$_settings['twilio']['number'] = '+12158743846';
+$_settings['twilio']['account_sid'] = getenv('account_sid') ?? '';
+$_settings['twilio']['auth_token'] = getenv('auth_token') ?? '';
+$_settings['twilio']['number'] = getenv('number') ?? '';
 
 //'heroku' for production, 'debug' for running locally
 $_settings['deploy_mode'] = 'heroku';
 
 if ($_settings['deploy_mode'] == 'heroku') {
-	$clear_db_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-	$_settings['db']['hostname']= $clear_db_url["host"];
-	$_settings['db']['database']= substr($clear_db_url["path"], 1);
-	$_settings['db']['username']= $clear_db_url["user"];
-	$_settings['db']['password']= $clear_db_url["pass"];
+	$_db_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+	$_settings['db']['hostname']= $_db_url["host"];
+	$_settings['db']['database']= substr($_db_url["path"], 1);
+	$_settings['db']['username']= $_db_url["user"];
+	$_settings['db']['password']= $_db_url["pass"];
 } else {
 	$_settings['db']['hostname']= "localhost";
 	$_settings['db']['database']= "firstaide_web";
