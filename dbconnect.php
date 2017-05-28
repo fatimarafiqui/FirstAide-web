786955e2b05bdac845da71ef1f24c28a5a8801d3
fatimarafiqui <fatima.rafiqui@gmail.com>
2017-05-29 00:24:08 +0530

    Adding database credentials for heroku

--- dbconnect.php
+++ dbconnect.php
@@ -3,9 +3,10 @@
   Desc: Connection to mysql db for use in other files*/
     
     //change the credentials here according to the server used
-	$username = "root";
-	$password="";
-	$hostname="localhost";
-	$database="firstaide_web";
-	$connection = mysqli_connect($hostname,$username,$password,$database)or die("Connection failed");
+	$connection = mysqli_connect(
+		$_settings['db']['hostname'],
+		$_settings['db']['username'],
+		$_settings['db']['password'],
+		$_settings['db']['database']
+	) or die("Connection failed");
 ?>
--- includes/settings.php
+++ includes/settings.php
@@ -6,12 +6,27 @@
  * Note       : Please do not commit your credentials
  */
 $_settings['reCaptcha'] = array();
-$_settings['reCaptcha']['client_key'] = '';
-$_settings['reCaptcha']['server_key'] = '';
+$_settings['reCaptcha']['client_key'] = '6LeAOhUUAAAAAKxj0kEhUUdxTGw4SQI-7LbTNHso';
+$_settings['reCaptcha']['server_key'] = '6LeAOhUUAAAAAO4R5aYPuOXlmqlIILm9FWjHtjTK';
 
 $_settings['twilio'] = array();
-$_settings['twilio']['account_sid'] = '';
-$_settings['twilio']['auth_token'] = '';
-$_settings['twilio']['number'] = '';
+$_settings['twilio']['account_sid'] = 'AC02b5e7eff09d776e4fa15963cbdecf8e';
+$_settings['twilio']['auth_token'] = '6d8d7004108d2ff1fcb4dccbb357edcb';
+$_settings['twilio']['number'] = '+12158743846';
 
+//'heroku' for production, 'debug' for running locally
+$_settings['deploy_mode'] = 'heroku';
+
+if ($_settings['deploy_mode'] == 'heroku') {
+	$clear_db_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+	$_settings['db']['hostname']= $clear_db_url["host"];
+	$_settings['db']['database']= substr($clear_db_url["path"], 1);
+	$_settings['db']['username']= $clear_db_url["user"];
+	$_settings['db']['password']= $clear_db_url["pass"];
+} else {
+	$_settings['db']['hostname']= "localhost";
+	$_settings['db']['database']= "firstaide_web";
+	$_settings['db']['username']= "root";
+	$_settings['db']['password']= "";
+}
 ?>
